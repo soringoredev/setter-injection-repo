dfb1e71266b713f3d1f95bc5c1d5e773432ffab9
Sorin <soringoredev@gmail.com>
2026-05-23 13:36:48 +0300

    Prepare project for setter injection

---
+++ src/main/java/com/example/demo/common/ItalianChef.java
@@ -0,0 +1,13 @@
+package com.example.demo.common;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ItalianChef implements Chef{
+    @Override
+    public String getDailyRecipe() {
+        return "Prepare pasta al pomodoro with fresh tomatoes and basil!";
+
+    }
+}
---
+++ src/main/java/com/example/demo/rest/DemoController.java
@@ -0,0 +1,25 @@
+package com.example.demo.rest;
+
+
+
+import com.example.demo.common.Chef;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    private Chef myChef;
+
+    //constructor
+    @Autowired
+    public DemoController(Chef theChef) {
+        myChef = theChef;
+    }
+
+    @GetMapping("/dailyrecipe")
+    public String getDailyRecipe() {
+        return myChef.getDailyRecipe();
+    }
+}
